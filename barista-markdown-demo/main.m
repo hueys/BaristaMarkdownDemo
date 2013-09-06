1756cc35317bab81d1a43ec54f006212a9003e1b
Steven Huey <shuey@artandlogic.com>
2013-09-06 18:53:30 -0400

    Xcode Project Template
    A brand new CLI app template project file and related files.

---
+++ barista-markdown-demo/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  barista-markdown-demo
+//
+//  Created by Steven Huey on 9/6/13.
+//  Copyright (c) 2013 Steven Huey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+   @autoreleasepool
+   {
+
+      // insert code here...
+      NSLog(@"Hello, World!");
+
+   }
+   
+   return 0;
+}
+
